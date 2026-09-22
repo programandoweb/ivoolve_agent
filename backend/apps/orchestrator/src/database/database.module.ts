3d70d209191724ec11708c5d95d70559e9003bc3
Jorge Méndez <lic.jorgemendez@gmail.com>
2026-09-22 09:53:24 -0500

    feat(trace): expose tracing globally

--- backend/apps/orchestrator/src/database/database.module.ts
+++ backend/apps/orchestrator/src/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 
 import { DatabaseService } from './database.service';
+import { ExecutionTraceService } from './execution-trace.service';
 
 @Global()
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+  providers: [DatabaseService, ExecutionTraceService],
+  exports: [DatabaseService, ExecutionTraceService],
 })
 export class DatabaseModule {}
